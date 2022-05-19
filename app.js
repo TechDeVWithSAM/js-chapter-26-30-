56530155fc332b7c2dfc5b4903d9faf75968da8b
Salman <Salmanrajputsr71@gmail.com>
2022-05-19 15:48:40 -0700

    salman

---
+++ app.js
@@ -0,0 +1,77 @@
+                             ///  Chapter 26 - 30 Subject  ///
+
+//  Question No 01 Start  //
+
+//    var num = 3.45214
+//    var num1 = Math.round(3.45214);
+//    var num2 = Math.floor(3.45214);
+//    var num3 = Math.ceil(3.45214);
+//    document.write("Number:"+ num + "</br>" + "Round of value:"+ num1+ "</br>"+ "Floor value of the Number:" +
+//    num2 +"</br>"+"Ceil Value of the Number:"+ num3);
+
+//  Question No 01 End  //
+
+//  Question No 02 Start  //
+
+//    var num = -2.673;
+//    var num1 = Math.round(-2.673);
+//    var num2 = Math.floor(-2.673);
+//    var num3 = Math.ceil(-2.673);
+//    document.write("Number:"+ num + "</br>" + "Round of floor:"+ num1+ "</br>"+ "Floor Value of the Number:" +
+//    num2 + "</br>"+"Ceil Value of the Number:"+ num3);
+
+//  Question No 02 End  //
+
+//  Question No 03 Start  //
+
+// var num = -4;
+// var abs = Math.abs(num)
+// document.write(`The Absolute value of ${num} is ${abs}`)
+
+//  Question No 03 End  //
+
+//  Question No 04 Start  //
+
+//   var num = Math.random(4,6);
+//   document.write(num);
+
+//  Question No 04 End  //
+
+//  Question No 05 Start  //
+
+//  var head = 1;
+//  var tail = 2;
+
+//  var toss = Math.random() *2;
+//  var floor = Math.floor (toss)
+//  if (floor === 0) {
+//     document.write("2 </br> Random Coin Value: Head")
+//  }else if(floor === 1)
+//  {
+//     document.write("1 </br> Random Coin Value: Tail")
+//  }
+
+//  Question No 05 End  //
+
+//  Question No 06 Start  //
+
+//   var num = Math.random("Random Number between 1 and 100" + 1,100);
+//   document.write(num);
+
+//  Question No 06 End  //
+
+//  Question No 07 Start  //
+
+// var user = (prompt("Enter your weight in kilograms"));
+// document.write(`The weight of user is ${user} kilograms`)
+
+//  Question No 07 End  //
+
+//  Question No 08 Start  //
+
+
+// var num = Math.random(1, 10);
+// document.write(num)
+
+
+         //  Question No 08 End  //
